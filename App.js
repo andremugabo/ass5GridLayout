a7f2954005c643e91dc74df374f241b27a45c94a
andremugabo <andremugabo@yahoo.fr>
2023-07-01 00:37:22 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, StyleSheet, Dimensions, Image } from "react-native";
 
-export default function App() {
+const SCREEN_WIDTH_THRESHOLD = 600;
+
+const data = [
+  require("./assets/splash.png"),
+  require("./assets/splash.png"),
+  require("./assets/splash.png"),
+  require("./assets/splash.png"),
+  require("./assets/splash.png"),
+  require("./assets/splash.png"),
+];
+
+const screenWidth = Dimensions.get("window").width;
+const columnCount = screenWidth > SCREEN_WIDTH_THRESHOLD ? 4 : 2;
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {data.map((item, index) => (
+        <Image source={item} style={styles.item} key={index} />
+      ))}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    gap: 5,
+  },
+  item: {
+    width: `${100 / columnCount}%`,
+    height: 100,
+    borderRadius: 5,
+    backgroundColor: "#ccc",
   },
 });
+
+export default App;
